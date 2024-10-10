4f363f5ef47bb2b0bf2a4c4c122660c06488247d
Coda <yuanzhida@yonyou.com>
2024-10-10 15:32:38 +0800

    init

---
+++ src/components/Comp1.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comp1: React.FC = function () {
+    return (
+        <>
+            <h3>1</h3>
+            <button>累加</button>
+        </>
+    )
+}
+export default Comp1;
