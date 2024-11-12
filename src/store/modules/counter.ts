8d52dd64787b1ac833a81d5ba5678ea03cc78ed8
coda <1035138801@qq.com>
2024-11-12 16:27:30 +0800

    APP搭建前一系列配置

--- src/store/modules/counter.ts
+++ src/store/modules/counter.ts
@@ -1,13 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface IState {
+  count: number
+  message: string
+}
+
+const initialState: IState = {
+  count: 100,
+  message: 'Hello Redux'
+}
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: {
-    count: 100,
-    message: 'Hello Redux'
-  },
+  initialState,
   reducers: {
-    changeMessageAction(state, { payload }) {
+    changeMessageAction(state, { payload }: PayloadAction<string>) {
       state.message = payload
     },
     increment: (state) => {
